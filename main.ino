f348e95e3e5fa2910166161cc26b93458b8ce5cb
Melan <alexleander91@gmail.com>
2022-10-29 16:40:59 +0200

    Call to initwifi() before calling mqtt_reconnect and enable ArduinoOTA

--- main.ino
+++ main.ino
@@ -211,6 +211,8 @@ void mqtt_callback(char* topic, byte* payload, unsigned int length) {
 		scroll_text_char(0, 30, buffer, length, 0, 255, 0);
 }
 
+
+
 void setup() {
 	Serial.begin(115200);
 
@@ -232,6 +234,29 @@ void setup() {
 
 	scroll_text(0, 30, WiFi.localIP().toString(), 0, 255, 0);
 
+	ArduinoOTA.setHostname("doorpixel");
+	
+	ArduinoOTA.onStart([]() {
+    	Serial.println("Start");
+ 	 });
+	
+	ArduinoOTA.onEnd([]() {
+		Serial.println("\nEnd");
+	});
+  
+    ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    	Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  	});
+  	ArduinoOTA.onError([](ota_error_t error) {
+		Serial.printf("Error[%u]: ", error);
+		if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+		else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+		else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+		else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+		else if (error == OTA_END_ERROR) Serial.println("End Failed");
+	});
+
+  	ArduinoOTA.begin();
 	mqttClient.setServer("mqtt.vm.nurd.space", 1883);
 	mqttClient.setCallback(mqtt_callback);
 
@@ -257,7 +282,9 @@ void setup() {
 
 void loop() {
 	if (!mqttClient.connected()) {
+		initWiFi();
 		mqtt_reconnect(); 
+		
 
 		if (mqttClient.connected()) {
 			mqttClient.publish("doorpixel/hello-version", __DATE__ " " __TIME__);
@@ -268,4 +295,5 @@ void loop() {
 	}
 
 	mqttClient.loop();
+	ArduinoOTA.handle();
 }
